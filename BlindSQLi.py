7c9cc9b4755145e862759f339801419b5d6378b1
Hamish Starling <37508609+starswap@users.noreply.github.com>
2021-09-22 16:46:47 +0100

    Update BlindSQLi.py

--- BlindSQLi.py
+++ BlindSQLi.py
@@ -1,6 +1,22 @@
+
 #Blind SQL Injection Tool for ethical penetration testing
+#    Copyright (C) 2021  Hamish Starling
+
+# This program is free software: you can redistribute it and/or modify
+#     it under the terms of the GNU General Public License as published by
+#     the Free Software Foundation, either version 3 of the License, or
+#     (at your option) any later version.
+
+#     This program is distributed in the hope that it will be useful,
+#     but WITHOUT ANY WARRANTY; without even the implied warranty of
+#     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#     GNU General Public License for more details.
+
+#     You should have received a copy of the GNU General Public License
+#     along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+
 #Created to solve an immersive labs challenge
-#Hamish Starling
 #Built specifically for MySQL but could be adapted
 
 import requests
